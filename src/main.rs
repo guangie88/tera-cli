39071ce41bedede828ca835e5448b242e3bc84df
Chen Weiguang <chen.weiguang@gmail.com>
2019-04-11 20:35:22 +0800

    Change to arg grouping

--- src/main.rs
+++ src/main.rs
@@ -1,94 +1,99 @@
-use clap::arg_enum;
+// #[macro_use]
+// extern crate serde_derive;
+
+// use clap::arg_enum;
 use std::{
     error::Error,
     fs,
     io::{self, Read},
     path::PathBuf,
 };
-use structopt::StructOpt;
+use structopt::{clap::ArgGroup, StructOpt};
 use tera::{Context, Tera};
 use toml::Value;
 
 type DynError = Box<dyn Error>;
+type CliResult<T> = Result<T, DynError>;
 
-#[derive(StructOpt, Debug)]
-#[structopt(name = "input", about = "Tera CLI input type")]
-enum Input {
-    #[structopt(name = "file", about = "Read from file")]
-    File {
-        /// File path to read template content from
-        #[structopt(parse(from_os_str))]
-        path: PathBuf,
-    },
-
-    #[structopt(name = "str", about = "Read directly from argument / STDIN")]
-    Str {
-        /// Template content to parse. Reads from STDIN if left empty.
-        #[structopt()]
-        content: Option<String>,
-    },
+fn input_arg_group() -> ArgGroup<'static> {
+    ArgGroup::with_name("input").required(true)
 }
 
-arg_enum! {
-    #[derive(Debug)]
-    enum ContextType {
-        Toml,
-    }
+fn format_arg_group() -> ArgGroup<'static> {
+    ArgGroup::with_name("format").required(true)
+}
+
+#[derive(StructOpt, Debug)]
+#[structopt(raw(group = "input_arg_group()"))]
+struct Input {
+    /// File path to read template content from
+    #[structopt(
+        name = "file",
+        long,
+        short,
+        group = "input",
+        parse(from_os_str)
+    )]
+    file: Option<PathBuf>,
+
+    /// Read directly from argument / STDIN
+    #[structopt(name = "str", long, short, group = "input")]
+    string: Option<String>,
+}
+
+#[derive(StructOpt, Debug)]
+#[structopt(raw(group = "format_arg_group()"))]
+struct ContextFormat {
+    #[structopt(name = "toml", long, group = "format", parse(from_os_str))]
+    toml: Option<PathBuf>,
+
+    #[structopt(name = "toml", long, group = "format")]
+    env: bool,
 }
 
 #[derive(StructOpt, Debug)]
 #[structopt(name = "args", about = "Tera CLI arguments")]
 struct Args {
-    #[structopt(subcommand)]
+    #[structopt(flatten)]
     input: Input,
 
-    #[structopt(raw(
-        possible_values = "&ContextType::variants()",
-        case_insensitive = "true"
-    ))]
-    context: ContextType,
-
-    #[structopt(parse(from_os_str))]
-    context_path: Option<PathBuf>,
+    #[structopt(flatten)]
+    context: ContextFormat,
 
     /// Root key to embed the context configuration into.
     #[structopt(short = "r", long = "root", default_value = "c")]
     root_key: String,
 }
 
-fn main() -> Result<(), DynError> {
+fn read_template(conf: &Args) -> CliResult<String> {
+    if let Some(ref path) = conf.input.file {
+        Ok(fs::read_to_string(&path)?)
+    } else if let Some(ref content) = conf.input.string {
+        Ok(content.clone())
+    } else {
+        let mut buffer = String::new();
+        io::stdin().read_to_string(&mut buffer)?;
+        Ok(buffer)
+    }
+}
+
+fn read_context(conf: &Args) -> CliResult<Context> {
+    if let Some(ref path) = conf.context.toml {
+        let value = fs::read_to_string(path)?.parse::<Value>()?;
+        let mut context = Context::new();
+        context.insert(&conf.root_key, &value);
+        Ok(context)
+    } else {
+        // else if conf.context.env
+        Ok(Context::new())
+    }
+}
+
+fn main() -> CliResult<()> {
     let conf = Args::from_args();
 
-    let template = match conf.input {
-        Input::File { path } => fs::read_to_string(&path)?,
-        Input::Str { content } => {
-            if let Some(content) = content {
-                content
-            } else {
-                let mut buffer = String::new();
-                io::stdin().read_to_string(&mut buffer)?;
-                buffer
-            }
-        }
-    };
-
-    let context = match conf.context {
-        ContextType::Toml => {
-            let context_path = if let Some(context_path) = conf.context_path {
-                context_path
-            } else {
-                PathBuf::from(".tera.toml")
-            };
-
-            let content = fs::read_to_string(&context_path)?;
-            let value = content.parse::<Value>()?;
-
-            let mut context = Context::new();
-            context.insert(&conf.root_key, &value);
-            context
-        }
-    };
-
+    let template = read_template(&conf)?;
+    let context = read_context(&conf)?;
     let rendered = Tera::one_off(&template, &context, true)?;
     print!("{}", rendered);
 
