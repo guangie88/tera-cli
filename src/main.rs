bc3c90abcd6cbac0d99bacd2c1b5cd70d464bc32
Chen Weiguang <chen.weiguang@gmail.com>
2019-04-11 11:39:25 +0800

    Raw working example for TOML

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,19 @@
-#[macro_use]
-extern crate structopt;
-
 use std::{
     error::Error,
     fs,
     io::{self, Read},
     path::PathBuf,
 };
+use clap::arg_enum;
 use structopt::StructOpt;
 use tera::{Context, Tera};
+use toml::Value;
 
 type DynError = Box<dyn Error>;
 
 #[derive(StructOpt, Debug)]
-#[structopt(name = "mode", about = "Tera CLI mode type")]
-enum Mode {
+#[structopt(name = "input", about = "Tera CLI input type")]
+enum Input {
     #[structopt(name = "file", about = "Read from file")]
     File {
         /// File path to read template content from
@@ -30,19 +29,32 @@ enum Mode {
     },
 }
 
+arg_enum! {
+    #[derive(Debug)]
+    enum ContextType {
+        Toml,
+    }
+}
+
 #[derive(StructOpt, Debug)]
 #[structopt(name = "args", about = "Tera CLI arguments")]
 struct Args {
     #[structopt(subcommand)]
-    mode: Mode,
+    input: Input,
+
+    #[structopt(raw(possible_values = "&ContextType::variants()", case_insensitive = "true"))]
+    context: ContextType,
+
+    #[structopt(parse(from_os_str))]
+    context_path: Option<PathBuf>,
 }
 
 fn main() -> Result<(), DynError> {
     let conf = Args::from_args();
 
-    let template = match conf.mode {
-        Mode::File { path } => fs::read_to_string(&path)?,
-        Mode::Str { content } => {
+    let template = match conf.input {
+        Input::File { path } => fs::read_to_string(&path)?,
+        Input::Str { content } => {
             if let Some(content) = content {
                 content
             } else {
@@ -53,7 +65,23 @@ fn main() -> Result<(), DynError> {
         }
     };
 
-    let context = Context::new();
+    let context = match conf.context {
+        ContextType::Toml => {
+            let context_path = if let Some(context_path) = conf.context_path {
+                context_path
+            } else {
+                PathBuf::from(".tera.toml")
+            };
+
+            let content = fs::read_to_string(&context_path)?;
+            let value = content.parse::<Value>()?;
+
+            let mut context = Context::new();
+            context.insert("c", &value);
+            context
+        }
+    };
+
     let rendered = Tera::one_off(&template, &context, true)?;
     print!("{}", rendered);
 
